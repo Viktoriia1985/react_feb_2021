6c684a2e1ab34da4a1eb2f793064f5a89bff1367
Viktoriia Krat <flightsspb@gmail.com>
2021-05-10 21:53:46 +0300

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 // import logo from './logo.svg';
 import './App.css';
 import Users from "./components/users/Users";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
 
 function App() {
     return (
         <div>
 
-            <Users/>
+            {/*<Users/>*/}
 
+            <Router>
+                <Link to={'/users'}>to users</Link>
+                <br/>
+                <Link to={'/posts'}>to posts</Link>
+                <Switch>
+                    <Route path={'/users'} render={() => (<Users/>)}/>
+                    <Route path={'/posts'} render={() => (<posts/>)}/>
+
+                </Switch>
+            </Router>
 
         </div>
     );
 }
 
 export default App;
+
+
